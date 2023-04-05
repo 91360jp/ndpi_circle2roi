e4e91d849206fc181f93e4a3895275b2b654f2b6
Masaharu Kobayashi <masakoby@gmail.com>
2023-04-05 15:10:34 +0900

    Add files via upload

---
+++ test400.bat
@@ -0,0 +1,14 @@
+::set dname_in=D:\20230315\DrWu\ndpi56\ndpi56\
+::set dname_in_ndpa=D:\20230315\DrWu\ndpi56\ndpi56\
+set dname_in=%1\
+set dname_in_ndpa=%dname_in%
+
+set gamma=1.8
+set ratio=3.0
+set width=400
+set dname_out=%dname_in%output%width%\
+
+mkdir %dname_out%
+
+.\bin\Release\ndpi_circle2roi.exe %dname_in% %dname_in_ndpa% %dname_out% %gamma% %ratio% %width%
+
